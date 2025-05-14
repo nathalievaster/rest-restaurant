86f434f25218ef11281065e1418c6e6785af1348
Nathalie Fredriksson <nfredriksson1@gmail.com>
2025-05-14 08:55:20 +0200

    installation av router

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 require("dotenv").config();
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
+
+// Routes
+app.use("/api", authRoutes);
 
 app.listen(port, () => {
     console.log(`Servern körs på http://localhost:${port}`);
