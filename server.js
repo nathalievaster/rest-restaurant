3e384e7c34e1c9f48a2778ca256c25d16b9ad0c7
Nathalie Fredriksson <nfredriksson1@gmail.com>
2025-05-14 08:51:31 +0200

    import av paket

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+    console.log(`Servern körs på http://localhost:${port}`);
+});
