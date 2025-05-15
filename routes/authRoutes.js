9a95c93ac4d6bccaf3cff5830a778f37368b0b58
Nathalie Fredriksson <nfredriksson1@gmail.com>
2025-05-15 07:44:33 +0200

    grund för logga in användare

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,23 @@
 const express = require ("express");
 const router = express.Router();
+const sqlite3 = require("sqlite3").verbose();
 
 
 // Logga in admin
 router.post("/login", async (req, res) => {
-    console.log ("login called...");
+    try {
+        const { username, password } = req.body;
+
+        // Validera input
+        if(!username || !password) {
+            return res.status(400).json({ error: "Ogiltig input, fyll i både användarnamn och lösenord"});
+        }
+
+        // Kolla om det är rätt
+        
+    } catch {
+        res.status(500).json({ error: "Server error"});
+    }
 });
 
 module.exports = router;
