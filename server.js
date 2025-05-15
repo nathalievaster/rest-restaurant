11b596a1ddb03d02a4efebb824b0ab2c0ba86e9d
Nathalie Fredriksson <nfredriksson1@gmail.com>
2025-05-15 08:25:51 +0200

    jwt för admins

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 const authRoutes = require("./routes/authRoutes");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,6 +10,26 @@ app.use(bodyParser.json());
 
 // Routes
 app.use("/api", authRoutes);
+
+// Skyddad admin-sida
+app.get("/api/editmenu", authenticteToken, (req, res) => {
+    res.json({ message: "Endast för admins!" });
+});
+
+// Validera token
+function authenticteToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if(token == null) res.status(401).json({ message: "Behörighet saknas för denna sida!"});
+
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, username) => {
+        if(err) return res.status(403).json({ message: "Ogiltig JWT" });
+
+        req.username = username;
+        next();
+    });
+}
 
 app.listen(port, () => {
     console.log(`Servern körs på http://localhost:${port}`);
